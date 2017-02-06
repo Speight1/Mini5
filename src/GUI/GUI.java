c7f67a2ab524dfb05b003f240a24db7efb99c798
Matthew <Matthew@192.168.1.16>
2017-02-06 23:19:58 +0000

    Set Up

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package events;
+package GUI;
 
 /**
  *
  * @author Matthew
  */
-public class Event {
+public class GUI {
     
 }
--- src/Network/Network.java
+++ src/Network/Network.java
@@ -3,16 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Events;
+package Network;
 
 /**
  *
  * @author Matthew
  */
-public class events {
+public class Network {
     
-    public static void main(String[] args){
-        int i = 1;
-        
-    }   
 }
